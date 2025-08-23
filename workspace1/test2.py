59751b871953e3fd481d4675fe513c714fb4ee9d
praveen <inboxmepraveen@gmail.com>
2025-08-23 10:31:59 +0530

    updated file

--- workspace1/test2.py
+++ workspace1/test2.py
@@ -92,3 +92,30 @@ print(current)
 
 print(datetime.date.today())
 print(datetime.datetime(2026,11,9,12,00))
+print()
+
+df = pd.DataFrame({
+    "Order_Date": ["2024-01-01", "2024-01-05", "2024-02-10"],
+    "Sales": [200, 150, 300]
+})
+
+# Convert to datetime
+df["Order_Date"] = pd.to_datetime(df["Order_Date"])
+print(df.dtypes)
+
+df["Yeer"]=df["Order_Date"].dt.year                                                                                                         
+df["Month"]=df["Order_Date"].dt.month                                                                                                         
+df["day"]=df["Order_Date"].dt.day
+df["day_name"]=df["Order_Date"].dt.day_name()
+
+print(df)
+
+print(df[df["Order_Date"]>"2024-02-01"])
+
+dates = pd.date_range(start="2024-01-01", end="2024-01-07", freq="D")
+print(dates)
+
+df["Days_Since"] = df["Order_Date"] - df["Order_Date"].min()
+print(df)
+
+df.to_excel("updated_sales.xlsx",index=False)
--- workspace1/text4.py
+++ workspace1/text4.py
@@ -1,9 +1,26 @@
 import datetime
+import pandas as pd
 current=datetime.datetime.now()
 print(current)
 
 print(datetime.date.today())
 print(datetime.datetime(2026,11,9,12,00))
 print()
-jhuhhvbhkhjbkj
-ssg
+
+df = pd.DataFrame({
+    "Order_Date": ["2024-01-01", "2024-01-05", "2024-02-10"],
+    "Sales": [200, 150, 300]
+})
+
+# Convert to datetime
+df["Order_Date"] = pd.to_datetime(df["Order_Date"])
+print(df.dtypes)
+
+df["Yeer"]=df["Order_Date"].dt.year                                                                                                         
+df["Month"]=df["Order_Date"].dt.month                                                                                                         
+df["day"]=df["Order_Date"].dt.day
+df["day_name"]=df["Order_Date"].dt.day_name()
+
+print(df)
+
+print(df[df["Order_Date"]>"2024-02-01"])
